cb35a152f301803d4d2e7a48676c63c66506ae1a
KL_jiana <2436262516@qq.com>
2025-03-13 00:55:54 +0800

    🛻初始提交，新建文件夹？？！！！

---
+++ src/main/java/org/hismeo/examples/event/ForgeEvent.java
@@ -0,0 +1,8 @@
+package org.hismeo.examples.event;
+
+import org.hismeo.examples.CrystalFracture;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = CrystalFracture.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ForgeEvent {
+}
