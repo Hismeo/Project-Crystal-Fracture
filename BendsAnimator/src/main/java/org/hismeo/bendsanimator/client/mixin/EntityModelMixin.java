27f146e2f2c528ddec6e4010cee0f0b141a469b2
KL_jiana <2436262516@qq.com>
2025-03-18 23:52:44 +0800

    🤨加个mixin

--- BendsAnimator/src/main/java/org/hismeo/bendsanimator/client/mixin/EntityModelMixin.java
+++ BendsAnimator/src/main/java/org/hismeo/bendsanimator/client/mixin/EntityModelMixin.java
@@ -1,16 +1,22 @@
 package org.hismeo.bendsanimator.client.mixin;
 
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HumanoidModel.class)
 public class EntityModelMixin<T extends LivingEntity> {
+    @Shadow @Final public ModelPart head;
+
     @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "HEAD"), cancellable = true)
     public void cancelAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci){
         ci.cancel();
+        this.head.xRot
     }
 }
