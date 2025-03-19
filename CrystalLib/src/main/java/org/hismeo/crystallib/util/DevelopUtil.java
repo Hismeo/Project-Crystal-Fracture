2a57a64a5abd12d1cc2796c6819e99d57b34c2d9
westernat <westernat233@163.com>
2025-03-19 20:52:10 +0800

    添加两个StructurePiece

--- CrystalLib/src/main/java/org/hismeo/crystallib/util/DevelopUtil.java
+++ CrystalLib/src/main/java/org/hismeo/crystallib/util/DevelopUtil.java
@@ -1,10 +1,13 @@
 package org.hismeo.crystallib.util;
 
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.fml.loading.FMLLoader;
 
 public class DevelopUtil {
     public static boolean isDev() {
-        return !FMLLoader.isProduction();
+        return !FMLEnvironment.production;
+    }
+
+    public static void devRun(Runnable runnable) {
+        if (isDev()) runnable.run();
     }
 }
--- HazeDungeon/src/main/java/org/hismeo/hazedungeon/HazeDungeon.java
+++ HazeDungeon/src/main/java/org/hismeo/hazedungeon/HazeDungeon.java
@@ -1,8 +1,10 @@
 package org.hismeo.hazedungeon;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.hismeo.hazedungeon.worldgen.structure.ModStructures;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +14,10 @@ public class HazeDungeon {
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
     public HazeDungeon(){
         IEventBus modBusEvent = FMLJavaModLoadingContext.get().getModEventBus();
+        ModStructures.PIECE_TYPES.register(modBusEvent);
+    }
+
+    public static ResourceLocation asResource(String path) {
+        return new ResourceLocation(MODID, path);
     }
 }
