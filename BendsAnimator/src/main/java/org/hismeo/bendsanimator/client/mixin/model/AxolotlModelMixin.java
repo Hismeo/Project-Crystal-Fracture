e247661c951c5561a827f48297a58443967874de
KL_jiana <2436262516@qq.com>
2025-04-06 14:11:42 +0800

    🛻清理重复mixin

--- BendsAnimator/src/main/java/org/hismeo/bendsanimator/client/mixin/model/AxolotlModelMixin.java
+++ BendsAnimator/src/main/java/org/hismeo/bendsanimator/client/mixin/model/AxolotlModelMixin.java
@@ -1,7 +1,8 @@
 package org.hismeo.bendsanimator.client.mixin.model;
 
-import net.minecraft.client.model.AxolotlModel;
+import net.minecraft.client.model.*;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.entity.EnderDragonRenderer;
 import org.hismeo.bendsanimator.client.api.IEntityModelGetter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -9,7 +10,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AxolotlModel.class)
+@Mixin(value = {AxolotlModel.class,
+        BeeModel.class,
+        BoatModel.class,
+        ChickenModel.class,
+        EnderDragonRenderer.DragonModel.class,
+        ElytraModel.class,
+        FoxModel.class,
+        HoglinModel.class,
+        HorseModel.class,
+        LlamaModel.class,
+        OcelotModel.class,
+        RabbitModel.class,
+        RaftModel.class,
+        ShulkerModel.class,
+        TadpoleModel.class,
+        WolfModel.class
+})
 public class AxolotlModelMixin implements IEntityModelGetter {
     @Unique
     private ModelPart root;
