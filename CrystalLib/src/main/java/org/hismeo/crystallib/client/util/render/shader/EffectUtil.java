ab2f8d9e4693d1ab27469911efb5549e967c6baf
KL_jiana <2436262516@qq.com>
2025-04-08 00:12:08 +0800

    🖊️修改包名

--- CrystalLib/src/main/java/org/hismeo/crystallib/client/util/render/shader/EffectUtil.java
+++ CrystalLib/src/main/java/org/hismeo/crystallib/client/util/render/shader/EffectUtil.java
@@ -1,10 +1,10 @@
-package org.hismeo.crystallib.client.util.render;
+package org.hismeo.crystallib.client.util.render.shader;
 
 import com.mojang.blaze3d.shaders.Uniform;
 
 import static org.hismeo.crystallib.client.util.MinecraftUtil.*;
 
-public class ShaderUtil {
+public class EffectUtil {
     public static Uniform getUniforms(int pass, String name){
         return getGameRenderer().currentEffect().passes.get(pass).getEffect().getUniform(name);
     }
