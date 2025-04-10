6f630a021aa7ea6715f982ff1907eca77c0202f4
KL_jiana <2436262516@qq.com>
2025-04-10 19:37:10 +0800

    🛻泥土屏幕，暂且开发，terraguns见

--- FractureClient/src/main/java/org/hismeo/fractureclient/event/ForgeEvent.java
+++ FractureClient/src/main/java/org/hismeo/fractureclient/event/ForgeEvent.java
@@ -1,53 +1,54 @@
 package org.hismeo.fractureclient.event;
 
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.hismeo.crystallib.client.util.render.BackGroundUtil;
 import org.hismeo.fractureclient.FractureClient;
 
-import static org.hismeo.crystallib.client.util.MinecraftUtil.getMinecraft;
-import static org.hismeo.crystallib.client.util.render.BackGroundUtil.renderBlurredBackground;
+import java.util.Arrays;
+
+import static org.hismeo.crystallib.client.util.MinecraftUtil.*;
 
 @Mod.EventBusSubscriber(modid = FractureClient.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ForgeEvent {
     @SubscribeEvent
     public static void screenEventRenderPost(ScreenEvent.Render.Post event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
     }
 
     @SubscribeEvent
     public static void screenEventRenderPre(ScreenEvent.Render.Pre event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
     }
 
     @SubscribeEvent
     public static void screenEventRenderInventoryMobEffects(ScreenEvent.RenderInventoryMobEffects event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
     }
 
     @SubscribeEvent
     public static void screenEventInitPre(ScreenEvent.Init.Pre event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
     }
 
     @SubscribeEvent
     public static void screenEventInitPost(ScreenEvent.Init.Post event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
+
     }
 
     @SubscribeEvent
     public static void screenEventClosing(ScreenEvent.Closing event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
     }
 
     @SubscribeEvent
     public static void screenEventOpening(ScreenEvent.Opening event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
     }
 
     @SubscribeEvent
     public static void screenEventBackgroundRendered(ScreenEvent.BackgroundRendered event) {
-//        renderBlurredBackground(getMinecraft().getPartialTick());
+        Screen currentScreen = event.getScreen();
+        GuiGraphics guiGraphics = event.getGuiGraphics();
+        BackGroundUtil.panoramaUtil(currentScreen, getPartialTick(), getMinecraft().level, guiGraphics, currentScreen.width, currentScreen.height);
     }
 }
