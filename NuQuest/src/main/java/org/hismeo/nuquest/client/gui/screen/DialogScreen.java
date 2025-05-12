4c6d124dc204bd34676118aebf6a2aa984887083
KalebLangley <2436262516@qq.com>
2025-05-12 23:32:37 +0800

    🖊️对之前数据的屏幕补齐罢了

--- NuQuest/src/main/java/org/hismeo/nuquest/client/gui/screen/DialogScreen.java
+++ NuQuest/src/main/java/org/hismeo/nuquest/client/gui/screen/DialogScreen.java
@@ -6,17 +6,31 @@ import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.hismeo.nuquest.api.dialog.text.ITextEffect;
+import org.hismeo.nuquest.core.dialog.SoundGroup;
 import org.hismeo.nuquest.core.dialog.context.DialogDefinition;
+import org.hismeo.nuquest.core.dialog.text.DialogText;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hismeo.crystallib.client.util.MinecraftUtil.*;
+
 public class DialogScreen extends Screen {
     protected final List<Button> optionalButton = new ArrayList<>();
     private final DialogDefinition dialogDefinition;
+    private final String dialogueId;
+    private final DialogText[] dialogTexts;
+    private String title;
+    private ResourceLocation imagePath;
+    private String text;
+    private SoundGroup soundGroup;
+    private ITextEffect textEffect;
     private final int maxPage;
     private int page;
+    private int initPage = -1;
 
     public DialogScreen(DialogDefinition dialogDefinition) {
         this(dialogDefinition, 0);
@@ -24,15 +38,18 @@ public class DialogScreen extends Screen {
 
     public DialogScreen(DialogDefinition dialogDefinition, int page) {
         super(CommonComponents.EMPTY);
-        this.dialogDefinition = dialogDefinition;
-        this.maxPage = dialogDefinition.dialogTexts().length;
         this.page = page;
+        this.maxPage = dialogDefinition.dialogTexts().length;
+        this.dialogDefinition = dialogDefinition;
+        this.dialogueId = dialogDefinition.dialogueId();
+        this.dialogTexts = dialogDefinition.dialogTexts();
     }
 
     @Override
     protected void init() {
         ImageButton flipButton = this.addRenderableWidget(new ImageButton(this.width - 40, this.height - 40, 20, 20, 0, 0, 20, Button.ACCESSIBILITY_TEXTURE, 32, 64, this::tryFlip));
         optionalButton.add(flipButton);
+        this.initPage();
     }
 
     @Override
@@ -40,16 +57,17 @@ public class DialogScreen extends Screen {
         int dialogueHeight = this.height / 3 * 2;
         guiGraphics.fillGradient(0, dialogueHeight, this.width, this.height, -1073741824, -1073741824);
 
-        String title = dialogDefinition.dialogTexts()[page].title();
-        String text = dialogDefinition.dialogTexts()[page].text();
         //  + page * this.font.lineHeight
-        guiGraphics.drawString(this.font, Component.translatable(title), 10, dialogueHeight + 4, 0xFFFFFFFF);
-        guiGraphics.fill(8, dialogueHeight + 4 + 10, 8 + 4 + this.font.width(title), dialogueHeight + 5 + 10, 0xFFFFFFFF);
-        guiGraphics.drawString(this.font, Component.translatable(text), 20, dialogueHeight + 25, 0xFFFFFFFF);
-        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        guiGraphics.blit(this.imagePath, 0, dialogueHeight - 64, 0, 0, 64, 64, 64, 64);
+        if (title != null) {
+            guiGraphics.drawString(this.font, Component.translatable(this.title), 10, dialogueHeight + 4, 0xFFFFFFFF);
+            guiGraphics.fill(8, dialogueHeight + 4 + 10, 8 + 4 + this.font.width(title), dialogueHeight + 5 + 10, 0xFFFFFFFF);
+        }
+        guiGraphics.drawString(this.font, Component.translatable(this.text), 20, dialogueHeight + 25, 0xFFFFFFFF);
         if (!canFlip()) {
             optionalButton.forEach(button -> button.active = false);
         }
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
     }
 
     @Override
@@ -58,11 +76,26 @@ public class DialogScreen extends Screen {
     }
 
     protected void tryFlip(Button button) {
-        if (canFlip()) this.page++;
+        if (canFlip()) {
+            this.page++;
+            this.initPage();
+        }
     }
 
     protected boolean canFlip() {
         return this.page < this.maxPage - 1;
+    }
+
+    protected void initPage() {
+        if (initPage != page) {
+            this.title = dialogTexts[page].title();
+            this.imagePath = dialogTexts[page].imagePath();
+            this.text = dialogTexts[page].text();
+            this.soundGroup = dialogTexts[page].soundGroup();
+            this.textEffect = dialogTexts[page].textEffect();
+            if (soundGroup != null) soundGroup.playSound(getLevel());
+            initPage = page;
+        }
     }
 }
 
--- NuQuest/src/main/java/org/hismeo/nuquest/core/dialog/SoundGroup.java
+++ NuQuest/src/main/java/org/hismeo/nuquest/core/dialog/SoundGroup.java
@@ -1,8 +1,12 @@
 package org.hismeo.nuquest.core.dialog;
 
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.hismeo.crystallib.client.util.MinecraftUtil;
 
 public class SoundGroup {
     private final SoundEvent soundEvent;
@@ -29,5 +33,15 @@ public class SoundGroup {
 
     public float getPitch() {
         return pitch;
+    }
+
+    public void playSound(Level level){
+        if (this.getSoundEvent() == null) return;
+        if (level.isClientSide){
+            LocalPlayer player = MinecraftUtil.getPlayer();
+            level.playSound(player, player.blockPosition(), this.soundEvent, SoundSource.PLAYERS, this.volume, this.pitch);
+        } else {
+            // 服务端，待处理
+        }
     }
 }
