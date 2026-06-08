19d2aacbe56f2e6c65c529bcbd6400214b47f8aa
KalebLangley <2436262516@qq.com>
2026-06-09 04:19:41 +0800

    celeritas

--- FractureClient/src/main/java/org/hismeo/fractureclient/client/render/screen/TitleScreen.java
+++ FractureClient/src/main/java/org/hismeo/fractureclient/client/render/screen/TitleScreen.java
@@ -2,10 +2,13 @@ package org.hismeo.fractureclient.client.render.screen;
 
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import org.embeddedt.embeddium.impl.gui.framework.DrawContext;
+import org.embeddedt.embeddium.impl.gui.framework.Renderable;
 
-public class TitleScreen extends Screen {
-    protected TitleScreen() {
-        //TODO langkey dategen
-        super(Component.translatable("screen.fracture_client.title_screen"));
+public class TitleScreen implements Renderable {
+
+    @Override
+    public void render(DrawContext drawContext, int mouseX, int mouseY, float partialTicks) {
+
     }
 }
